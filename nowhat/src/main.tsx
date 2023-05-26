d9f92f44b3701500324caeae52859f28e82fb1fb
StarWolf <leonardomf96@gmail.com>
2023-05-26 16:53:06 +0000

    make login routing

--- nowhat/src/main.tsx
+++ nowhat/src/main.tsx
@@ -2,11 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <App />,
+  },
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
